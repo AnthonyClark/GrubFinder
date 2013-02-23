3801846fa73c215295b484ba64175ad3890564e9
Anthony <anthonyclark.ac@gmail.com>
2013-02-23 03:37:46 -0500

    Adding in Places search support
    getGrub is not yet called anywhere, and there are still some bugs
    leaving placeForGrub undefined.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var map;
 var userPosition = new google.maps.LatLng();
+var maxDistance = String(2000);
 var geocoder;
 var userInput = document.getElementById('addrSearch');
+var infowindow;
+var placeForGrub;
 
 function initialize() {
 
@@ -68,4 +71,39 @@ function codeAddress() {
 }
 
 function geoLocFail() {
+}
+
+function getGrub() {
+
+    var request = {
+        location: userPosition,
+        radius: maxDistance,
+        types: ['food','restaurant','meal_delivery','meal_takeaway']
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, randomSpot);
+}
+
+function randomSpot(grubResults, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        var i = (Math.random() * (grubResults.length - 1));
+        placeForGrub = grubResults[i];
+        createMarker(placeForGrub);
+    }
+    else {
+        alert("Status NOT OKAY from Places Request.");
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
 }
