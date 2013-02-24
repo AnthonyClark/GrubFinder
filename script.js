3a6edf83ee0cf2324d4a1e1109fd3283a84df862
Anthony <anthonyclark.ac@gmail.com>
2013-02-23 21:54:18 -0500

    Refactor of  scripting begun and Form changed

--- script.js
+++ script.js
@@ -1,12 +1,110 @@
+var init_done = false;
 var map;
 var userPosition = new google.maps.LatLng();
 var maxDistance = String(2000);
 var geocoder;
-var userInput = document.getElementById('addrSearch');
 var infowindow;
 var placeForGrub;
 
 // Make the autocomplete work for address search
+
+var PI = {
+
+    onReady: function () {
+        console.log("onReady");
+        GEO.getNavLoc();
+        $("#searchForm").submit(PI.searchHandler);
+
+    },
+
+    searchHandler: function () {
+        if (!init_done) {
+            MAP.initialize(
+                GEO.addrToLatLng( $("#search").val() )
+            );
+        }
+    }
+
+}
+
+var GEO = {
+    addrToLatLng: function (addr) {
+
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': addr }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                console.log("addrToLatLng status okay");
+                MAP.initialize( results[0].geometry.location);
+            } else {
+                console.log("GeocoderStatus is NOT OK");
+                return;
+            }
+        });
+    },
+
+    latLngToAddr: function (pos) {
+
+        console.log("latLngToAddr");
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'latLng': pos }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var str = results[0].formatted_address;
+
+                // Setting the search value here, be careful!
+                $("#search").val(str)
+
+                return str;
+            } else {
+                console.log("GeocoderStatus is NOT OK");
+                return;
+            }
+        });
+
+    },
+
+    getNavLoc : function () {
+        console.log("getNavLoc");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                geoLocSuccess,
+                geoLocFail,
+                { timeout: 10000 }
+            );
+        } else {
+            console.log("Geolocation from the browser failed");
+            return;
+        }
+
+        function geoLocSuccess(position) {
+
+            console.log("geoLocSuccess");
+            var navLoc = new google.maps.LatLng(
+                    position.coords.latitude,
+                    position.coords.longitude
+            );
+
+            GEO.latLngToAddr(navLoc);
+        }
+    }
+}
+
+var MAP = {
+
+    initialize: function (init_pos) {
+
+        var mapOptions = {
+            center: init_pos,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    }
+
+}
+
+
+$(document).ready(PI.onReady);
 
 function initialize() {
 
